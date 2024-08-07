1e4638803888f75aec9846eaef472034b888748c
khushbu-s-shah <141067682+khushbu-s-shah@users.noreply.github.com>
2024-08-06 23:28:45 -0600

    change in structure

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <header className='bg-gradient-to-n from-green-400 via-blue-500 to-purple-600 shadow-md py-4'>
+    <header className='bg-gray-800 shadow-md py-4'>
         <div className="container mx-auto flex justify-between items-center px-4">
             <Link href='/' className='text-3xl font-bold text-white'>Global Flavor Hub</Link>
         </div>  
